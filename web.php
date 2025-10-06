b011d931b97b7138d4e841dc4c4f92412eba1545
nannaphatklo <nannaphat.klo@kkumail.com>
2025-10-07 02:28:57 +0700

    Add files via upload

--- web.php
+++ web.php
@@ -1,17 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ManageAccountController;
+use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -22,34 +15,22 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name("dashboard");
+    
+    Route::get('/dashboard/manage', [ManageAccountController::class, 'index'])->name("ManageAccount");
+    Route::get('/dashboard/manage/{id}/edit', [ManageAccountController::class, 'edit'])->name("Manage.edit");
+    Route::post('/dashboard/manage/{id}/update', [ManageAccountController::class, 'update'])->name("Manage.update");
+    Route::get('/dashboard/manage/{id}/delete', [ManageAccountController::class, 'destroy'])->name("Manage.destroy");
+    
+    Route::get('/dashboard/profile', [DashboardController::class, 'profile'])->name("profile");
+    Route::get('/dashboard/profile/{id}/edit', [DashboardController::class, 'profile_edit'])->name("profile.edit");
+    Route::post('/dashboard/profile/{id}/update', [DashboardController::class, 'profile_update'])->name("profile.update");
+    Route::get('/dashboard/profile/{id}/delete', [DashboardController::class, 'profile_destroy'])->name("profile.destroy");
+    
+    
 });
 
-
-use App\Http\Controllers\EventPublicController;
-use App\Http\Controllers\BookingController;
-use App\Http\Controllers\SouvenirOrderController;
-
-
-// รายการอีเวนต์ (อนุมัติแล้ว — ในที่นี้ถือว่าอยู่ในตาราง events แล้ว)
-Route::get('/events', [EventPublicController::class,'indexEvent'])->name('home');
-Route::get('/events/{id}', [EventPublicController::class,'showEvent'])->name('events.showEvent');
-
-// ต้องล็อกอินก่อนจอง
-Route::middleware(['auth','verified'])->group(function () {
-    // จองตั๋ว
-    Route::post('/events/{id}/book', [BookingController::class,'storeZone'])->name('bookings.storeZone');
-    Route::get('/mybookings', [BookingController::class,'indexBooking'])->name('bookings.indexBooking');
-    Route::post('/bookings/{id}/updateZone', [BookingController::class,'updateZone'])->name('bookings.updateZone');
-    Route::post('/bookings/{id}/destroy', [BookingController::class,'destroy'])->name('bookings.destroy');
-
-    // จองของที่ระลึก 
-    Route::get('/mysouvenirs/history', [SouvenirOrderController::class, 'history'])->name('souvenirs.showSouvenir'); // ประวัติการจอง
-    Route::get('/mysouvenirs', [SouvenirOrderController::class, 'indexSouvenir'])->name('souvenirs.indexSouvenir'); // แสดงรายการ
-    Route::post('/souvenirs/{id}/order', [SouvenirOrderController::class, 'store'])->name('souvenirs.store'); // 
-    Route::post('/souvenirs/{id}/destroy', [SouvenirOrderController::class, 'destroy'])->name('souvenirs.destroy'); // ยกเลิก
-});
-
-
+Route::post('/logout', function () {
+    Auth::logout();
+    return redirect()->route('login');
+})->name('logout');
