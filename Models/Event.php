d2dce0f15d69aed99f70b08f88baedb6a791fe07
CKprtt <peerathat105@gmail.com>
2025-10-07 16:06:41 +0700

    Add files via upload

--- Models/Event.php
+++ Models/Event.php
@@ -2,13 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Event extends Model {
-    use SoftDeletes;
-    protected $primaryKey = 'events_id';
+class Event extends Model
+{
+    use HasFactory, SoftDeletes;
 
-    public function eventRequests() { return $this->hasMany(EventRequest::class,'events_id'); }
-    public function ticketBookings() { return $this->hasMany(TicketBooking::class,'events_id'); }
+    protected $primaryKey = 'events_id';
+    protected $fillable = [
+        'events_name',
+        'description',
+        'poster_path',
+        'start_date',
+        'end_date',
+        'type_hall',
+    ];
+
+    public function eventRequests()
+    {
+        return $this->hasMany(EventRequest::class, 'events_id', 'events_id');
+    }
+
+    public function ticketBookings()
+    {
+        return $this->hasMany(TicketBooking::class, 'events_id', 'events_id');
+    }
 }
